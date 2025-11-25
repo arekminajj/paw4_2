a3c572b0ac9f90055f94948ee44895390d73542c
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-11-25 11:10:12 +0100

    Com4_2.1be

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const { categories, funnyJoke, lameJoke } = require("./jokes");
 
-app.get('/math/circle/:r', (req, res) => {
-  
-  const PI = 3.14
+app.get("/math/circle/:r", (req, res) => {
+  const PI = 3.14;
   const r = Number(req.params.r);
 
   const area = PI * r * r;
   const circumference = 2 * PI * r;
 
-  const result = {"area": area,"circumference": circumference}
+  const result = { area: area, circumference: circumference };
   res.json(result);
 });
 
-app.get('/math/rectangle/:w/:h', (req, res) => {
-  
+app.get("/math/rectangle/:w/:h", (req, res) => {
   const w = Number(req.params.w);
   const h = Number(req.params.h);
 
   const area = w * h;
   const perimiter = 2 * w + 2 * h;
 
-  const result = {"area": area,"perimiter": perimiter}
+  const result = { area: area, perimiter: perimiter };
   res.json(result);
 });
 
-app.get('/math/power/:base/:exponent', (req, res) => {
+app.get("/math/power/:base/:exponent", (req, res) => {
   const base = Number(req.params.base);
   const exponent = Number(req.params.exponent);
-  const passRoot = req.query.root === 'true';
+  const passRoot = req.query.root === "true";
 
   if (Number.isNaN(base)) {
     return res.status(400).json({ error: "invalid input." });
@@ -50,6 +49,46 @@ app.get('/math/power/:base/:exponent', (req, res) => {
   return res.json({ result });
 });
 
+app.get("/jokebook/categories", (req, res) => {
+  return res.json({ categories });
+});
+
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+app.get("/jokebook/joke/:category", (req, res) => {
+  const category = req.params.category;
+
+  let jokes;
+
+  if (category === "funnyJoke") {
+    jokes = funnyJoke;
+  } else if (category === "lameJoke") {
+    jokes = lameJoke;
+  } else {
+    return res.json({ error: `no jokes for category ${category}` });
+  }
+
+  const randomJoke = pickRandom(jokes);
+  return res.json(randomJoke);
+});
+
+// wlasny endpoint z losowym zartem z losowej kategorii.
+app.get("/jokebook/random", (req, res) => {
+  const category = pickRandom(categories);
+
+  let jokes;
+
+  if (category === "funnyJoke") {
+    jokes = funnyJoke;
+  } else if (category === "lameJoke") {
+    jokes = lameJoke;
+  }
+
+  const randomJoke = pickRandom(jokes);
+  return res.json({ category, ...randomJoke });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ jokes.js
@@ -0,0 +1,38 @@
+let categories = ["funnyJoke", "lameJoke"];
+
+let funnyJoke = [
+  {
+    joke: "Dlaczego komputer poszedł do lekarza?",
+
+    response: "Bo złapał wirusa!",
+  },
+
+  {
+    joke: "Dlaczego komputer nie może być głodny?",
+
+    response: "Bo ma pełen dysk!",
+  },
+
+  {
+    joke: "Co mówi jeden bit do drugiego?",
+
+    response: "„Trzymaj się, zaraz się przestawiamy!”",
+  },
+];
+
+let lameJoke = [
+  {
+    joke: "Dlaczego programiści preferują noc?",
+
+    response: "Bo w nocy jest mniej bugów do łapania!",
+  },
+
+  {
+    joke: "Jak nazywa się bardzo szybki programista?",
+
+    response: "Błyskawiczny kompilator!",
+  },
+];
+
+module.exports = { categories, funnyJoke, lameJoke };
+
